5f684c779c8d549730318f403e6cbfba1607beee
iwtw <wwttww@qq.com>
2017-10-16 17:59:09 +0800

    modified:   perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -7,32 +7,40 @@ N2 = 400
 DIM = 2 + 1 
 
 class Perceptron:
-    def __init__( w ):
-        self.w = np.random.randn( DIM )
-    def loss ( w , x , y  ):
-        "denoting N the number of samples , M the dimension of each sample"
-        "w shapes [M]"
-        "x shapes [M,N] , the augmented matrice"
-        "y shapes [N] "
+    def __init__( self ):
+        self.w = np.random.randn( DIM , 1  )
+    def loss (  self,x , y  ):
+        "w shapes [DIM,1]"
+        "x shapes [DIM,N] , the augmented matrice of training samples "
+        "y shapes [N,1] , the labels"
         M , N = x.shape()
-        w = w.reshape( (M,1) )
-        x = x.reshape( (M,N) )
+        x = x.reshape( (DIM,N) )
         y = y.reshape( (N,1) )
-        return y.dot( w.T).dot(x) 
-    def generate_data(  ):
-        x1 = np.ones( ( DIM , N1 ) )
-        x2 = np.ones ( ( DIM , N2 ) )
-
-        x1[0,:] = -1.7 + 1.1 * np.random.randn(N1)
-        x1[1,:] = 1.6 + 0.9 * np.random.randn(N1)
-
-        x2[0,:] = 1.3 + 1.0 * np.random.randn(N2)
-        x2[1,:] = -1.5 + 0.8 * np.random.randn(N2)
+        return y.dot( self.w.T).dot(x) 
+    def grad (self, x , y):
+        return x.dot(y)
+    def upadte ( self,x , y  ):
+        self.w += -1.0 * LEARNING_RATE * grad(x,y)
         
-        y1 = np.ones((N1))
-        y2 = -1*np.ones((N2))
-        return x1 , y1 ,  x2 , y2
+
+def generate_data(  ):
+    x1 = np.ones( ( DIM , N1 ) )
+    x2 = np.ones ( ( DIM , N2 ) )
+
+    x1[0,:] = -1.7 + 1.1 * np.random.randn(N1)
+    x1[1,:] = 1.6 + 0.9 * np.random.randn(N1)
+
+    x2[0,:] = 1.3 + 1.0 * np.random.randn(N2)
+    x2[1,:] = -1.5 + 0.8 * np.random.randn(N2)
+    
+    y1 = np.ones((N1))
+    y2 = -1 * np.ones((N2))
+    return x1 , y1 ,  x2 , y2
 
 if __name__ == "__main__" :
     x1 , y1 ,  x2 , y2  = generate_data()
+    perceptron = Perceptron()
+    for it in range(1000):
+        perceptron.update()
+
 
