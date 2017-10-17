9b3e99cc75d61afd5fe3e889fc4b3cad77da75ae
iwtw <wwttww@qq.com>
2017-10-17 18:51:26 +0800

    modified:   perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,27 +1,32 @@
 import numpy as np
-import matlabplot as plot
+import matplotlib.pyplot as plot
 
 LEARNING_RATE = 1e-6
 N1 = 440
 N2 = 400
 DIM = 2 + 1 
 
+
 class Perceptron:
     def __init__( self ):
         self.w = np.random.randn( DIM , 1  )
     def loss (  self,x , y  ):
-        "w shapes [DIM,1]"
-        "x shapes [DIM,N] , the augmented matrice of training samples "
-        "y shapes [N,1] , the labels"
-        M , N = x.shape()
-        x = x.reshape( (DIM,N) )
-        y = y.reshape( (N,1) )
+        "
+        w shapes [DIM,1]
+        x shapes [DIM,N] , the augmented matrice of training samples 
+        y shapes [N,1] , the labels
+        "
+        _ , N = x.shape()
+      #  x = x.reshape( (DIM,N) )
+      #  y = y.reshape( (N,1) )
         return y.dot( self.w.T).dot(x) 
     def grad (self, x , y):
         return x.dot(y)
-    def upadte ( self,x , y  ):
-        self.w += -1.0 * LEARNING_RATE * grad(x,y)
-        
+    def update ( self,x , y  ):
+        self.w += -1.0 * LEARNING_RATE * self.grad(x,y)
+    def g(self,x):
+        return self.w.T.dot(x)
+
 
 def generate_data(  ):
     x1 = np.ones( ( DIM , N1 ) )
@@ -33,14 +38,28 @@ def generate_data(  ):
     x2[0,:] = 1.3 + 1.0 * np.random.randn(N2)
     x2[1,:] = -1.5 + 0.8 * np.random.randn(N2)
     
-    y1 = np.ones((N1))
-    y2 = -1 * np.ones((N2))
+    y1 = np.ones((N1,1))
+    y2 = -1 * np.ones((N2,1))
     return x1 , y1 ,  x2 , y2
 
 if __name__ == "__main__" :
     x1 , y1 ,  x2 , y2  = generate_data()
+    plot.axis( (-6,6,-6,6) )
+    plot.scatter(x1[0,:],x1[1,:],marker=".")
+    plot.scatter(x2[0,:],x2[1,:],marker=".")
     perceptron = Perceptron()
+    X = np.concatenate( [x1,x2] ,axis = 1 )
+    Y = np.concatenate( [y1,y2], axis = 0 )
     for it in range(1000):
-        perceptron.update()
+        perceptron.update( X , Y )
+    hyperplane = np.zeros((100,100))
+    for i_idx,i_v in enumerate (np.linspace(-6,6,100)):
+        for j_idx ,j_v in enumerate ( np.linspace(-6,6,100)):
+            g = perceptron.g( np.array( [i_v,j_v,1] ).T )
+            hyperplane[i_idx,j_idx] = g 
 
-
+    i = j = np.linspace(-6,6,100)
+    plot.contour(i,j,hyperplane,levels=[0])
+    #plot.contour(np.array( hyperplane).T,levels=[0])
+    #plot.scatter( hyperplane[0] , hyperplane[1],marker="." )
+    plot.show()
